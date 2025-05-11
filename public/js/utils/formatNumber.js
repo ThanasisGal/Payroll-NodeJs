55e09fd1da6f442b2d7d21ede860bb13940c1fdf
ThanasisGal <tgalanopoulos@gmail.com>
2025-05-11 13:46:06 +0100

    Auto-deploy 2025-05-11 15:46:06

--- public/js/utils/formatNumber.js
+++ public/js/utils/formatNumber.js
@@ -1,3 +1,5 @@
+// Γεμίζει μηδενικά προσθέτοντάς τα στην αρχή
+
 function formatNumber(number, totalLength) {
   // Μετατροπή του αριθμού σε string και προσθήκη προπορευόμενων μηδενικών με την padStart()
   return number.toString().padStart(totalLength, '0');
