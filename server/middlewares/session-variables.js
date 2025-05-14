d4e0ed56fe8c9895453fc05046cb46bafb522f0f
ThanasisGal <tgalanopoulos@gmail.com>
2025-05-14 19:35:12 +0100

    Auto-deploy 2025-05-14 21:35:11

--- server/middlewares/session-variables.js
+++ server/middlewares/session-variables.js
@@ -1,20 +1,26 @@
 var getSessionVars = async (req, res, next) =>{
-  if (req.session && req.session.userId) {
-    res.locals.userId = req.session.userId;
-    res.locals.userName = req.session.userName;
-    res.locals.userTeam = req.session.userTeam;
-    res.locals.userRole = req.session.userRole;
-    res.locals.userStatus = req.session.userStatus;
-    res.locals.companyInUse = req.session.companyInUse;
-    res.locals.companyDescription = req.session.companyDescription;
-    res.locals.yearInUse = req.session.yearInUse;
-    res.locals.periodInUse = req.session.periodInUse;
-    res.locals.periodInUseDescr = req.session.periodInUseDescr;
-    res.locals.appDate = req.session.appDate;
-  } else {
-    res.locals.userId = null;
+  console.log("getSessionVars running", req.session);
+  try {
+    if (req.session && req.session.userId) {
+      res.locals.userId = req.session.userId;
+      res.locals.userName = req.session.userName;
+      res.locals.userTeam = req.session.userTeam;
+      res.locals.userRole = req.session.userRole;
+      res.locals.userStatus = req.session.userStatus;
+      res.locals.companyInUse = req.session.companyInUse;
+      res.locals.companyDescription = req.session.companyDescription;
+      res.locals.yearInUse = req.session.yearInUse;
+      res.locals.periodInUse = req.session.periodInUse;
+      res.locals.periodInUseDescr = req.session.periodInUseDescr;
+      res.locals.appDate = req.session.appDate;
+    } else {
+      res.locals.userId = null;
+    }
+    next();
+  } catch (err) {
+    console.error("Error in getSessionVars:", err);
+    next(err); // ή res.status(500).send("Something went wrong")
   }
-  next();
 };
 
 export default getSessionVars
